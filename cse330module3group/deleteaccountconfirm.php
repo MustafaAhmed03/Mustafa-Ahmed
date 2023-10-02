c53707026f53ec0c412f4cb2559156e8c8832f8c
Mustafa Ahmed <89863472+MustafaAhmed03@users.noreply.github.com>
2023-10-01 19:12:59 -0500

    Create deleteaccountconfirm.php

---
+++ cse330module3group/deleteaccountconfirm.php
@@ -0,0 +1,11 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <title>Delete Account</title>
+    <link rel="stylesheet" type="text/css" href="style.css">
+</head>
+<body>
+  <h2>Delete account?<h2>
+  <a href='deleteaccount.php'>Yes.</a>
+</body>
+</html>
